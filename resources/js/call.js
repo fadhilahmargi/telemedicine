d15a747c01e4672ed714bcb4481b8159ed7c4e4b
fadhilahmargi <fadhilahmargi@gmail.com>
2025-05-01 08:26:38 +0700

    last

--- resources/js/call.js
+++ resources/js/call.js
@@ -68,181 +68,202 @@ $(function () {
     });
 
     function muteMic() {
-        localVideo.srcObject.getAudioTracks().forEach(track => track.enabled = !track.enabled);
-    }
-
-    function muteCam() {
-        localVideo.srcObject.getVideoTracks().forEach(track => track.enabled = !track.enabled);
-    }
-
-    $(document).on('click', '#muteCamBtn', () => {
-        muteCam();
-        $(this).toggleClass('text-red-400');
-    });
-
-    $(document).on('click', '#muteMicBtn', () => {
-        muteMic();
-        $(this).toggleClass('text-red-400');
-    });
-
-    $(document).on('click', '#hangupBtn', () => {
-    });
-
-    function getUser(id = null) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/getUser',
-                type: 'GET',
-                data: { id: id },
-                success: function (response) {
-                    resolve(response);
-                },
-                error: function (error) {
-                    reject(error);
-                }
-            })
-        })
-    }
-
-    function showHideCallPopup(name, image, text, btns = true) {
-        $("#caller-popup-container").toggleClass('hidden');
-        $("#caller-name").text(`${name} ${text}`);
-        $("#caller-profileImage").attr('src', '/storage/' + image);
-        if (btns === false) {
-            $(".call-buttons").hide();
-        }
-    }
-
-    function send(type, data, receiverID, user) {
-        Echo.private(`chat.${receiverID}`).whisper('Webrtc', {
-            senderID: user.id || null,
-            senderName: user.name || null,
-            profileImage: user.profileImage || null,
-            recipientId: receiverID,
-            type: type,
-            data: data
-        })
-    }
-
-    async function createOffer(receiverID, user) {
-        await peerConnection.createOffer({
-            OfferToReceiveVideo: 1,
-            OfferToReceiveAudio: 0
-        });
-        await peerConnection.setLocalDescription(peerConnection.localDescription);
-        send('client-offer', peerConnection.localDescription, receiverID, user);
-        sendIceCandidate(receiverID);
-    }
-
-    async function createAnswer(receiverID, data) {
-        if (!peerConnection) {
-            await createConn();
+        if (peerConnection) {
+            localVideo.srcObject.getAudioTracks().forEach(track => {
+                track.enabled = !track.enabled;
+            });
         }
 
-        if (!localStream) {
-            await getCam();
-        }
-
-        await peerConnection.setRemoteDescription(data);
-        await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(peerConnection.localDescription);
-        send('client-answer', peerConnection.localDescription, receiverID, '');
-        sendIceCandidate(receiverID);
-    }
-
-    async function sendIceCandidate(receiverID) {
-        peerConnection.onicecandidate = event => {
-            if (event.candidate !== null) {
-                send('client-candidate', event.candidate, receiverID, '');
+        function muteCam() {
+            if (peerConnection) {
+                localVideo.srcObject.getVideoTracks().forEach(track => {
+                    track.enabled = !track.enabled;
+                });
             }
-        }
 
-        peerConnection.onicecandidatestatechange = (event) => {
-            if (peerConnection.iceConnectionState === 'disconnected' || peerConnection.iceConnectionState === 'failed') {
-                alert("The other user has disconnected.");
-                // setTimeout('window.location.reload(true)', 2000);
+            $(document).on('click', '#muteCamBtn', () => {
+                muteCam();
+                $(this).toggleClass('text-red-400');
+            });
+
+            $(document).on('click', '#muteMicBtn', () => {
+                muteMic();
+                $(this).toggleClass('text-red-400');
+            });
+
+            $(document).on('click', '#hangupBtn', () => {
+                getUser().then(function (data) {
+                    user = data;
+                    send('client-hangup', null, receiverID, user);
+                    peerConnection.close();
+                    peerConnection = null;
+                    location.reload(true);
+                }).catch(function (error) {
+                    console.error("Error getting user: ", error);
+                });
+            });
+
+            function getUser(id = null) {
+                return new Promise((resolve, reject) => {
+                    $.ajax({
+                        url: '/getUser',
+                        type: 'GET',
+                        data: { id: id },
+                        success: function (response) {
+                            resolve(response);
+                        },
+                        error: function (error) {
+                            reject(error);
+                        }
+                    })
+                })
             }
-        }
-    }
 
-    Echo.private(`chat.${authID}`).listenForWhisper('Webrtc', async (e) => {
-        let message = e;
-        console.log(message);
-        let type = message.type;
-        let data = message.data;
-        let receiver = message.recipientId;
-
-        let sender = {
-            id: message.senderID,
-            name: message.senderName,
-            profileImage: message.profileImage
-        };
-
-        switch (type) {
-            case 'client-candidate':
-                if (peerConnection.localDescription) {
-                    await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+            function showHideCallPopup(name, image, text, btns = true) {
+                $("#caller-popup-container").toggleClass('hidden');
+                $("#caller-name").text(`${name} ${text}`);
+                $("#caller-profileImage").attr('src', '/storage/' + image);
+                if (btns === false) {
+                    $(".call-buttons").hide();
                 }
-                break;
-            case 'is-client-ready':
+            }
+
+            function send(type, data, receiverID, user) {
+                Echo.private(`chat.${receiverID}`).whisper('Webrtc', {
+                    senderID: user.id || null,
+                    senderName: user.name || null,
+                    profileImage: user.profileImage || null,
+                    recipientId: receiverID,
+                    type: type,
+                    data: data
+                })
+            }
+
+            async function createOffer(receiverID, user) {
+                await peerConnection.createOffer({
+                    OfferToReceiveVideo: 1,
+                    OfferToReceiveAudio: 1
+                });
+                await peerConnection.setLocalDescription(peerConnection.localDescription);
+                send('client-offer', peerConnection.localDescription, receiverID, user);
+                sendIceCandidate(receiverID);
+            }
+
+            async function createAnswer(receiverID, data) {
                 if (!peerConnection) {
                     await createConn();
                 }
 
-                if (peerConnection.iceConnectionState === 'connected') {
-                    getUser(receiver).then(function (data) {
-                        user = data
-                        // send('client-already-oncall', null, receiverID, data);
-                    }).catch(function (error) {
-                        localStream
-                        console.error("Error getting user: ", error);
-                    });
-                } else {
-                    showHideCallPopup(sender.name, sender.profileImage, 'is calling');
-
-                    acceptBtn.on('click', function () {
-                        getUser(receiver).then(function (data) {
-                            showHideCallPopup(sender.name, sender.profileImage, '');
-                            send('client-is-ready', null, sender.id, data);
-                        }).catch(function (error) {
-                            localStream
-                            console.error("Error getting user: ", error);
-                        });
-                    });
-
-                    declineBtn.on('click', function () {
-                        getUser(receiver).then(function (data) {
-                            send('client-rejected', null, sender.id, data);
-                            location.reload(true);
-                        }).catch(function (error) {
-                            localStream
-                            console.error("Error getting user: ", error);
-                        });
-                    });
-
+                if (!localStream) {
+                    await getCam();
                 }
-                break;
-            case 'client-is-ready':
-                createOffer(receiverID, user);
-                break;
-            case 'client-offer':
-                createAnswer(sender.id, data);
-                showCall();
-                break;
-            case 'client-answer':
-                if (peerConnection.localDescription) {
-                    await peerConnection.setRemoteDescription(data);
+
+                await peerConnection.setRemoteDescription(data);
+                await peerConnection.createAnswer();
+                await peerConnection.setLocalDescription(peerConnection.localDescription);
+                send('client-answer', peerConnection.localDescription, receiverID, '');
+                sendIceCandidate(receiverID);
+            }
+
+            async function sendIceCandidate(receiverID) {
+                peerConnection.onicecandidate = event => {
+                    if (event.candidate !== null) {
+                        send('client-candidate', event.candidate, receiverID, '');
+                    }
                 }
-                break;
-            case 'client-already-oncall':
-                showHideCallPopup(sender.name, sender.profileImage, 'is on another call');
-                // setTimeout('window.location.reload(true)', 2000);
-                break;
-            case 'client-rejected':
-                showHideCallPopup(sender.name, sender.profileImage, 'is busy');
-                // setTimeout('window.location.reload(true)', 2000);
-                break;
+
+                peerConnection.onicecandidatestatechange = (event) => {
+                    if (peerConnection.iceConnectionState === 'disconnected' || peerConnection.iceConnectionState === 'failed') {
+                        alert("The other user has disconnected.");
+                        // setTimeout('window.location.reload(true)', 2000);
+                    }
+                }
+            }
+
+            Echo.private(`chat.${authID}`).listenForWhisper('Webrtc', async (e) => {
+                let message = e;
+                console.log(message);
+                let type = message.type;
+                let data = message.data;
+                let receiver = message.recipientId;
+
+                let sender = {
+                    id: message.senderID,
+                    name: message.senderName,
+                    profileImage: message.profileImage
+                };
+
+                switch (type) {
+                    case 'client-candidate':
+                        if (peerConnection.localDescription) {
+                            await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+                        }
+                        break;
+                    case 'is-client-ready':
+                        if (!peerConnection) {
+                            await createConn();
+                        }
+
+                        if (peerConnection.iceConnectionState === 'connected') {
+                            getUser(receiver).then(function (data) {
+                                user = data
+                                // send('client-already-oncall', null, receiverID, data);
+                            }).catch(function (error) {
+                                localStream
+                                console.error("Error getting user: ", error);
+                            });
+                        } else {
+                            showHideCallPopup(sender.name, sender.profileImage, 'is calling');
+
+                            acceptBtn.on('click', function () {
+                                getUser(receiver).then(function (data) {
+                                    showHideCallPopup(sender.name, sender.profileImage, '');
+                                    send('client-is-ready', null, sender.id, data);
+                                }).catch(function (error) {
+                                    localStream
+                                    console.error("Error getting user: ", error);
+                                });
+                            });
+
+                            declineBtn.on('click', function () {
+                                getUser(receiver).then(function (data) {
+                                    send('client-rejected', null, sender.id, data);
+                                    location.reload(true);
+                                }).catch(function (error) {
+                                    localStream
+                                    console.error("Error getting user: ", error);
+                                });
+                            });
+
+                        }
+                        break;
+                    case 'client-is-ready':
+                        createOffer(receiverID, user);
+                        break;
+                    case 'client-offer':
+                        createAnswer(sender.id, data);
+                        showCall();
+                        break;
+                    case 'client-answer':
+                        if (peerConnection.localDescription) {
+                            await peerConnection.setRemoteDescription(data);
+                        }
+                        break;
+                    case 'client-already-oncall':
+                        showHideCallPopup(sender.name, sender.profileImage, 'is on another call');
+                        // setTimeout('window.location.reload(true)', 2000);
+                        break;
+                    case 'client-rejected':
+                        showHideCallPopup(sender.name, sender.profileImage, 'is busy');
+                        // setTimeout('window.location.reload(true)', 2000);
+                        break;
+                    case 'client-hangup':
+                        showHideCallPopup(sender.name, sender.profileImage, 'is busy');
+                        // setTimeout('window.location.reload(true)', 2000);
+                        break;
+                }
+            });
+
         }
-    });
-
+    }
 });
